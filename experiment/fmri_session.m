b2535b9a24ef6cab06b3f90106c7271bdf72767a
DONG HEE LEE <4c4448@gmail.com>
2021-05-19 21:24:42 +0900

    Modify a file name

---
+++ experiment/fmri_session.m
@@ -0,0 +1,68 @@
+clc;
+clear;
+close all;
+%% SETTING
+addpath(genpath(pwd));
+%PATH = getenv('PATH');
+%setenv('PATH', [PATH ':/Users/donghee/anaconda3/bin:/Users/donghee/anaconda3/condabin']); %For biopack, you need to add your python3 path
+%setenv('PATH', [PATH ':/Library/Frameworks/Python.framework/Versions/3.7/bin']);
+
+basedir = pwd;
+
+expt_param.screen_mode = 'Full'; %{'Test','Full'}
+
+expt_param.run_name = 'run';
+expt_param.run_num = 01;
+
+expt_param.pathway = false;
+expt_param.dofmri = false;
+
+%% Experiment parameter
+expt_param.subjectID='test02'; %Subject ID
+
+loaddir = fullfile(pwd,'Data/calibration');
+fname = fullfile(loaddir,['cali_' expt_param.subjectID '.mat']);
+
+load(fname);
+
+% --
+whichScreen = 1; % you can check the screen number by running Screen('Screens')
+
+expt_param.cue_types = ["HighCue", "LowCue"];
+expt_param.cue_shapes = ["△", "☐"];
+
+%gLMS ; 
+%(1) barely detectable, 1.4;
+%(2) weak, 6;
+%(3) moderate, 17;
+%(4) strong, 34.7;
+%(5)very strong, 52.5;
+%(6) strongest imaginable sensation of any kind, 100
+LowPain = reg.FinalLMH_5Level(2);
+MidPain = reg.FinalLMH_5Level(3); 
+HighPain = reg.FinalLMH_5Level(4);
+expt_param.heat_intensity_table = [LowPain, MidPain, HighPain]; % stimulus intensity
+
+expt_param.condition_nums = 3;
+expt_param.trial_nums_per_condition = 10;
+expt_param.midpain_per_condition = 2;
+expt_param.trial_nums = expt_param.condition_nums * expt_param.trial_nums_per_condition;
+
+%expt_param.run_type = 'Plus_High_Certainty'; % {'Plus_High_Certainty', 'Plus_Middle_Certainty', 'Plus_Low_Certainty'}
+
+% {'no_movie_heat', 'movie_heat', 'caps', 'resting'}
+
+%% Start experiment
+data = data_save(expt_param, basedir);
+data.expt_param = expt_param;
+%data.dat.experiment_start_time = GetSecs; %이걸 왜 또하지 data_save()에서 했는데
+
+screen_param = setscreen(expt_param);
+
+explain(screen_param);
+  
+practice(screen_param, expt_param);
+
+data = run(screen_param, expt_param, data);
+  
+data = close(screen_param, data);

cd7fe5fea2f901fda208ac9a61fc7fbdc7a643e9
DONG HEE LEE <4c4448@gmail.com>
2021-05-19 21:21:47 +0900

    Feat a option for max heat intensity

--- experiment/practice.m
+++ experiment/practice.m
@@ -30,6 +30,7 @@ x = W*(1/4);
 y = H*(1/2);
 SetMouse(x,y)  
 
+
 %% Assign variables 2
 % Keyboard input setting
 if expt_param.dofmri
@@ -41,69 +42,63 @@ end
 %% Maximum temperature heat pain stimulus
 % Push the button to deliver heat stimulus
 
+if expt_param.max_heat
+    while true
+        if expt_param.dofmri
+            [~,~,keyCode] = KbCheck(apple);
+        else
+            [~,~,keyCode] = KbCheck(-1);
+        end
 
-while true
-    if expt_param.dofmri
-        [~,~,keyCode] = KbCheck(apple);
+    %     if keyCode(KbName('q')) == 1
+    %         abort_experiment('manual');
+    %         break
+    %     end
+
+        msgtxt = ['실험자는 \n\n 최고온도 열자극을 전달하려면 \n\n m 을 눌러주시기 바랍니다. \n\n (q : 화면 끄기)'];
+        msgtxt = double(msgtxt); % korean to double
+        DrawFormattedText(theWindow, msgtxt, 'center', 'center', white, [], [], [], 2);
+        Screen('Flip', theWindow); 
+
+
+        if keyCode(KbName('m')) == 1
+            break;
+        elseif keyCode(KbName('q')) == 1
+            abort_experiment('manual');
+            break
+        end
+    end
+
+
+    % Making pathway program list
+    PathPrg = load_PathProgram('HBM2021');
+
+    MaxHeat.program = PathPrg{45,4}; % 48 degree [48 '01000110' 'MPC_48' 70]
+    MaxHeat.intensity = 48;
+
+
+
+    %-------------Setting Pathway------------------
+    if expt_param.pathway
+        main(ip, port, 1, MaxHeat.program);     % Maximum temperature
+        WaitSecs(2);
+    %-------------Ready for Pathway------------------
+        main(ip, port, 2); %ready to pre-start
+        WaitSecs(2); 
+    %------------- start to trigger thermal stimulus------------------    
+        Screen(theWindow, 'FillRect', bgcolor, window_rect);
+        Screen('TextSize', theWindow, 60);
+        DrawFormattedText(theWindow, double('+'), 'center', 'center', white, [], [], [], 1.2);
+        Screen('Flip', theWindow);
+        Screen('TextSize', theWindow, fontsize);
+        main(ip,port,2);        
     else
-        [~,~,keyCode] = KbCheck(-1);
+        Screen(theWindow, 'FillRect', bgcolor, window_rect);
+        DrawFormattedText(theWindow, double(num2str(MaxHeat.intensity)), 'center', 'center', white, [], [], [], 1.2);
+        Screen('Flip', theWindow); 
     end
-    
-    if keyCode(KbName('q')) == 1
-        abort_experiment('manual');
-        break
-    end
-
-    msgtxt = ['실험자는 \n\n 최고온도 열자극을 전달하려면 \n\n m 을 눌러주시기 바랍니다. \n\n (q : 화면 끄기)'];
-    msgtxt = double(msgtxt); % korean to double
-    DrawFormattedText(theWindow, msgtxt, 'center', 'center', white, [], [], [], 2);
-    Screen('Flip', theWindow); 
-    
-    
-    if keyCode(KbName('m')) == 1
-        break;
-    elseif keyCode(KbName('q')) == 1
-        abort_experiment('manual');
-        break
-    end
+    WaitSecs(13);
 end
-
-
-% Making pathway program list
-PathPrg = load_PathProgram('HBM2021');
-
-MaxHeat.program = PathPrg{45,4}; % 48 degree [48 '01000110' 'MPC_48' 70]
-MaxHeat.intensity = 48;
-
-
-
-%-------------Setting Pathway------------------
-if expt_param.pathway
-    main(ip, port, 1, MaxHeat.program);     % Maximum temperature
-end
-WaitSecs(2);
-
-%-------------Ready for Pathway------------------
-if expt_param.pathway
-    main(ip, port, 2); %ready to pre-start
-end
-WaitSecs(2); 
-
-%------------- start to trigger thermal stimulus------------------    
-if expt_param.pathway
-    Screen(theWindow, 'FillRect', bgcolor, window_rect);
-    Screen('TextSize', theWindow, 60);
-    DrawFormattedText(theWindow, double('+'), 'center', 'center', white, [], [], [], 1.2);
-    Screen('Flip', theWindow);
-    Screen('TextSize', theWindow, fontsize);
-    main(ip,port,2);        
-else
-    Screen(theWindow, 'FillRect', bgcolor, window_rect);
-    DrawFormattedText(theWindow, double(num2str(MaxHeat.intensity)), 'center', 'center', white, [], [], [], 1.2);
-    Screen('Flip', theWindow); 
-end
-WaitSecs(13);
-
 %% Rating bar practice
 while true % To finish practice, push button
     msgtxt = '참가자는 충분히 평가 방법을 연습한 후 \n\n 연습이 끝나면 버튼을 눌러주시기 바랍니다.';
